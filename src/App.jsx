a7edf42cc6555b2bb9e32100ea0709d5a89b34de
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-04 12:37:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Shop from "./pages/Shop/Shop";
 
 import Shoping from "./pages/Shoping/Shoping";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 import { RestrictedRoute } from "./components/RestrictedRoute";
 import { PrivateRoute } from "./components/PrivateRoute";
@@ -14,23 +14,24 @@ import { PrivateRoute } from "./components/PrivateRoute";
 
 import RegisterForm from "./pages/RegisterForm/RegisterForm"
 import LoginForm from "./pages/LoginForm/LoginForm";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 
 
-import { selectedToken } from "./redux/auth/selectors";
-import { refresh } from "./redux/auth/operations";
+// import { selectedToken } from "./redux/auth/selectors";
+// import { refresh } from "./redux/auth/operations";
 import History from "./pages/History/History";
 import Home from "./pages/Home/Home";
 
+
 const App = () => {
-  const dispatch = useDispatch()
-  const token = useSelector(selectedToken)
-console.log(token)
-  useEffect(() => {
-    if (token) {
-      dispatch(refresh());
-    }
-  }, [dispatch, token])
+//   const dispatch = useDispatch()
+  // const token = useSelector(selectedToken)
+// console.log(token)
+//   useEffect(() => {
+//     if (token) {
+//       dispatch(refresh());
+//     }
+//   }, [dispatch, token])
   return (
     <>
       <Routes>
