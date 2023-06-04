7426705d065d1b1afe4065a3a8b2ca0a08b09539
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-04 03:44:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider  store={store}> 
     <PersistGate loading={null}
         persistor={persistor}>
-    <BrowserRouter basename="food_delivery">
+    <BrowserRouter basename="/">
     <App />
     </BrowserRouter>
     </PersistGate>
