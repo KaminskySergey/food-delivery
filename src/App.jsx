43c7ef948daffda66c8323cc2c230efdac6966c2
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-04 12:09:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ import Shoping from "./pages/Shoping/Shoping";
 
 import { useEffect } from "react";
 
-import { RestrictedRoute } from "./components/RestrictedRoute";
-import { PrivateRoute } from "./components/PrivateRoute";
+// import { RestrictedRoute } from "./components/RestrictedRoute";
+// import { PrivateRoute } from "./components/PrivateRoute";
 
 
-import RegisterForm from "./pages/RegisterForm/RegisterForm"
-import LoginForm from "./pages/LoginForm/LoginForm";
+// import RegisterForm from "./pages/RegisterForm/RegisterForm"
+// import LoginForm from "./pages/LoginForm/LoginForm";
 import { useDispatch, useSelector } from "react-redux";
 
 
 import { selectedToken } from "./redux/auth/selectors";
 import { refresh } from "./redux/auth/operations";
-import History from "./pages/History/History";
+// import History from "./pages/History/History";
 
 
 const App = () => {
@@ -38,10 +38,10 @@ console.log(token)
             <Route path="/shop" element={<Shop />} />
             <Route path="/shop/:shopId" element={<Shop />} />
             <Route path="/shoping" element={<Shoping />} />
-            <Route path="/orders" element={<PrivateRoute component={History} redirectTo={'/'} />} />
+            {/* <Route path="/orders" element={<PrivateRoute component={History} redirectTo={'/'} />} /> */}
             
-            <Route path="/login" element={<RestrictedRoute component={LoginForm} redirectTo={'/'} />} />
-            <Route path="/register" element={<RestrictedRoute component={RegisterForm} redirectTo={'/'} />} />
+            {/* <Route path="/login" element={<RestrictedRoute component={LoginForm} redirectTo={'/'} />} />
+            <Route path="/register" element={<RestrictedRoute component={RegisterForm} redirectTo={'/'} />} /> */}
         </Route>
       </Routes>
     </>
