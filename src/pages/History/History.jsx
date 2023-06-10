06ca3b2e3af862c537624f4c8037b2804584cee1
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-11 00:42:15 +0300

    Update History.jsx

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -24,7 +24,7 @@ useEffect(() => {
     return (
         <>
         <Box width={'45%'} ml='auto' mr='auto' pt={96}  style={{borderRadius: '30px', display: 'flex'}}>
-            <Box display='flex' p={16}  style={{outline: '1px solid black', minHeight: '600px', width: '100%', backgroundColor:'rgb(0 0 0 / 80%)', borderRadius: '32px'}}>
+            <Box p={16}  style={{outline: '1px solid black', minHeight: '600px', width: '100%', backgroundColor:'rgb(0 0 0 / 80%)', borderRadius: '32px'}}>
             {historyOrders?.length !== 0 ? <HistoryList>
                 <HistoryItem />
             </HistoryList> : 
